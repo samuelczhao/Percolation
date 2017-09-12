c2c15af6e23a92a32d7c6a4204eb7c2d8253bf3b
bhscs-build <bhs.cs.build@gmail.com>
2017-09-11 21:31:08 -0700

    Populating starter project: Part 2

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,27 @@
+public class Percolation
+{            
+    public Percolation(int n)
+    {
+    	throw new UnsupportedOperationException();
+    }
+
+    public void open(int i, int j)
+    {
+    	throw new UnsupportedOperationException();
+    }
+
+    public boolean isOpen(int i, int j)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isFull(int i, int j)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean percolates()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,27 @@
+public class PercolationStats
+{
+    public PercolationStats(int N, int T)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public double mean()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public double stddev()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public double confidenceLo()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public double confidenceHi()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
