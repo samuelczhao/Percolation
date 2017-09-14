df35e586b9c0083cec5760d2a7f6e5041923fe7c
samuelczhao <samuelczhao@10.104.41.73>
2017-09-14 08:05:47 -0700

    another

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -22,7 +22,7 @@ public class Percolation
     	
     	for (int i = 1; i <= n; i++)
     	{
-    		grid[1][n] = true;
+    		grid[1][i] = true;
     		union.union(top, i);
     		union.union(bottom, n * n + 1 - i);
     	}
